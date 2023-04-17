1615b26f06bf640da37e99eaa776c79ab55b0d79
repositorionicorm <repositorionicorm@gmail.com>
2023-04-16 19:12:12 -0500

    volviendo aprobar

--- src/main/java/com/wasicode02/wasicode/controller/VistaController.java
+++ src/main/java/com/wasicode02/wasicode/controller/VistaController.java
@@ -1,5 +1,6 @@
 package com.wasicode02.wasicode.controller;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
